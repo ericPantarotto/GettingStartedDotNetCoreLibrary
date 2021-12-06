01c568fd1fb9f713d4ea3fbf67ad018819c1fd86
Eric Carlier <eric.carlier@gmail.com>
2021-12-06 18:49:31 +0100

    Init Blazor Server Library

--- BlazorServerDALibrary/Models/PersonModel.cs
+++ BlazorServerDALibrary/Models/PersonModel.cs
@@ -6,7 +6,7 @@ namespace BlazorServerDALibrary.Models
 {
     public class PersonModel : IPersonModel
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
