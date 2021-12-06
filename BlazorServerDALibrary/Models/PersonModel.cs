7446f70b4c7c909b53c869b0c7d3a8775db67f9a
Eric Carlier <eric.carlier@gmail.com>
2021-12-06 22:18:33 +0100

    BSDA Library Init

--- BlazorServerDALibrary/Models/PersonModel.cs
+++ BlazorServerDALibrary/Models/PersonModel.cs
@@ -10,6 +10,5 @@ namespace BlazorServerDALibrary.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public string test { get; set; }
     }
 }
